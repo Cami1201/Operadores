626d5826360471b3d62636d610f6fa7b6ce839ad
Camila Perilla <prueba@correo.com>
2024-02-25 02:50:27 -0500

    Se realizaron las 8 operaciones en java

---
+++ src/main/java/co/edu/sena/oop_2902274/App.java
@@ -0,0 +1,13 @@
+package co.edu.sena.oop_2902274;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
